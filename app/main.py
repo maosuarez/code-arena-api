448338df5a078b5264c09d958722fbd70a121c8c
maosuarez <maosuarezbarrer@gmail.com>
2025-09-02 19:19:56 -0500

    Arreglar Cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from app.routes import auth, competition, users, teams
-from fastapi.security import OAuth2PasswordBearer
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Competencias Universitarias - Backend")
+
+# Lista explícita de orígenes permitidos (ajusta según tu frontend)
+origins = [
+    "https://code-arena-cegpgwdrfnfybufp.eastus2-01.azurewebsites.net",  # Producción
+    "http://localhost:3000",    # Desarrollo local
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,              # Orígenes permitidos
+    allow_credentials=True,             # Permitir cookies/autenticación
+    allow_methods=["*"],                # Métodos HTTP permitidos
+    allow_headers=["*"],                # Headers permitidos
+)
+
 
 # Incluir rutas
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
